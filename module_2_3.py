6de617fda695e79fa183e32e51fa6179b29821df
Timka <takhmeto2009@gmail.com>
2024-11-19 15:25:30 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+number = 0
+while number < len(my_list):
+    if my_list[number] > 0:
+        print(my_list[number])
+        number = number + 1
+    elif my_list[number] == 0:
+        number = number + 1
+        continue
+    else:
+        break
